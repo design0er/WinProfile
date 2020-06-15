6887ad97a32d85102c9fdfe2038d77dcc9eabf48
design0er@deb10_01 <design0er@gmail.com>
2020-06-15 12:35:15 +0800

    add "emacs-package,deb-pack"

---
+++ deb-package.sh
@@ -0,0 +1,19 @@
+#history|grep "apt install"
+apt install org-mode
+apt install ibus-rime
+apt install fonts-firacode
+apt install emacs-common-non-dfsg
+apt install xfonts-wqy fonts-wqy-microhei fonts-wqy-zenhei
+apt install rxvt-unicode-256color
+apt install ufw
+apt install tightvncserver
+apt install awesome
+apt install certbot
+apt install gnupg2
+apt install htop
+apt install net-tools
+apt install vim git htop tmux fish zsh emacs
+apt install fonts-powerline
+apt install python3-pip proxychians4
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+pip3 install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U
---
+++ emacs-package.el
@@ -0,0 +1,27 @@
+;;;for bootstrap emacs package system.
+;;;install package before use.
+;;;simple and stable.
+;;;set package archives
+(setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+						 ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(package-initialize)
+(package-refresh-contents)
+(setq my-packages '(use-package
+					 elpy
+					 magit
+					 evil
+					 evil-leader
+					 evil-tabs
+					 ivy
+					 swiper
+					 counsel
+					 org-mind-map
+					 org-bullets
+					 markdown-mode
+					 flymd
+					 flycheck
+					 elpy
+					 geiser
+					 paredit))
+(dolist (p my-packages) (print p))
+(dolist (p my-packages) (package-install p))
